e33ce91a49e2ca235c52a4d0334447a9c9916372
mayerxc <mayerxc@gmail.com>
2022-12-31 16:07:18 -0500

    add js

--- static/main.js
+++ static/main.js
@@ -1,7 +1,42 @@
-// fetch('https://example.com/profile', {
-//   method: 'POST', // or 'PUT'
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(data),
-// });
+const loadingDiv = document.querySelector('.loading');
+loadingDiv.style.visibility = 'hidden';
+
+function showSpinner() {
+  loadingDiv.style.visibility = 'visible';
+}
+
+function hideSpinner() {
+  loadingDiv.style.visibility = 'hidden';
+}
+
+async function makeImage(e) {
+  e.preventDefault();
+  showSpinner();
+  const prompt = document.querySelector('input').value;
+  const size = '512x512';
+  const data = { prompt, size, n: 1 };
+  const headers = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch('/make_image', headers);
+  const responseJson = await response.json();
+  const { url, error } = responseJson;
+  if (error) {
+    // display error on page for user if they typed something inappropriate
+    document.querySelector('.msg').textContent = error;
+    document.querySelector('#image').src = '';
+    document.querySelector('#message').textContent = prompt;
+  } else {
+    document.querySelector('.msg').textContent = '';
+    document.querySelector('#image').src = url;
+    document.querySelector('#message').textContent = prompt;
+  }
+  document.querySelector('input').value = '';
+  hideSpinner();
+}
+
+document.querySelector('button').addEventListener('click', makeImage);
