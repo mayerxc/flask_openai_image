e053d9f9cfc4ad2a1bdafff52d23b6f65e8a2c0a
Chris Mayer <mayerxc@gmail.com>
2023-01-10 18:50:23 -0500

    Add data elements to use in js file and add size

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
-const loadingDiv = document.querySelector('.loading');
+const loadingDiv = document.querySelector('[data-loading]');
+const inputElement = document.querySelector('[data-input]');
+const sizeElement = document.querySelector('[data-size]');
+const errorElement = document.querySelector('[data-error]');
+const messageElement = document.querySelector('[data-message]');
+const imageElement = document.querySelector('[data-image]');
+
 loadingDiv.style.visibility = 'hidden';
 
 function showSpinner() {
@@ -12,8 +18,8 @@ function hideSpinner() {
 async function makeImage(e) {
   e.preventDefault();
   showSpinner();
-  const prompt = document.querySelector('input').value;
-  const size = '512x512';
+  const prompt = inputElement.value;
+  const size = sizeElement.value;
   const data = { prompt, size, n: 1 };
   const headers = {
     method: 'POST',
@@ -28,23 +34,23 @@ async function makeImage(e) {
     const { url, error } = responseJson;
     if (error) {
       // display error on page for user if they typed something inappropriate
-      document.querySelector('.error-msg').textContent = error;
-      document.querySelector('#image').src = '';
-      document.querySelector('#message').textContent = prompt;
+      errorElement.textContent = error;
+      imageElement.src = '';
+      messageElement.textContent = prompt;
     } else {
-      document.querySelector('.error-msg').textContent = '';
-      document.querySelector('#image').src = url;
-      document.querySelector('#message').textContent = prompt;
+      errorElement.textContent = '';
+      imageElement.src = url;
+      messageElement.textContent = prompt;
     }
   } catch (err) {
     console.error('error', err);
-    document.querySelector('.error-msg').textContent = err;
-    document.querySelector('#image').src = '';
-    document.querySelector('#message').textContent = prompt;
+    errorElement.textContent = err;
+    imageElement.src = '';
+    messageElement.textContent = prompt;
   }
 
-  document.querySelector('input').value = '';
+  inputElement.value = '';
   hideSpinner();
 }
 
-document.querySelector('button').addEventListener('click', makeImage);
+document.querySelector('[data-button]').addEventListener('click', makeImage);
